acb8124b4814f1246adfb68ab86c4ea1d35fa1ff
user.name <jeremiebergeronlol@outlook.com>
2020-04-21 15:35:36 -0400

    Ajout TP7 - Test 4

--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -28,7 +28,7 @@ describe('ProblemeComponent', () => {
   it(' Zone PRÉNOM invalide avec 2 caractères', ()=>{
     let zone = component.problemeForm.controls['prenom'];
     zone.setValue('a'.repeat(2));
-    expect(zone.valid).toBeFalse();
+    expect(zone.valid).toBeFalsy();
   });
 
   it(' Zone PRÉNOM valide avec 3 caractères', ()=>{
@@ -41,5 +41,13 @@ describe('ProblemeComponent', () => {
     let zone = component.problemeForm.controls['prenom'];
     zone.setValue('a'.repeat(200));
     expect(zone.valid).toBeTrue();
+  });
+
+  it(' Zone PRÉNOM invalide avec aucune valeur', ()=>{
+    let errors= {};
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue(null);
+    errors = zone.errors || {};
+    expect(errors['minLength']).toBeFalsy();
   });
 });
