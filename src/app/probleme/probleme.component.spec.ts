c22b00c63c4797c61bd342f664e86f23e98afc60
user.name <jeremiebergeronlol@outlook.com>
2020-04-21 15:11:14 -0400

    Ajout TP7 - Test 3

--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -36,4 +36,10 @@ describe('ProblemeComponent', () => {
     zone.setValue('a'.repeat(3));
     expect(zone.valid).toBeTrue();
   });
+
+  it(' Zone PRÉNOM valide avec 200 caractères', ()=>{
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue('a'.repeat(200));
+    expect(zone.valid).toBeTrue();
+  });
 });
